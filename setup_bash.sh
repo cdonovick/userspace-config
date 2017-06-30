7c1ad5e27e687acc5722e8430085d5da70e45a8b
Caleb Donovick <donovick@cs.stanford.edu>
2017-06-30 15:14:17 -0700

    Backup

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 dir=$(pwd)
-
+files=(.bash_profile .bash_history .bash_logout .bashrc)
 cd ~/
-rm -f .bash_profile .bash_history .bash_logout .bashrc
+for f in "${files[@]}"; do 
+    rm -f ${f}.OLD
+    mv $f ${f}.OLD 2>/dev/null 
+done
+
 ln -s $dir/bash/bashrc.sh .bashrc
 ln -s $dir/bash/bash_profile.sh .bash_profile
 ln -s $dir/bash/bash_logout.sh .bash_logout

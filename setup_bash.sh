f7b8684b32a31348518079b83fec378499bdc7c9
Caleb Donovick <donovick@cs.stanford.edu>
2017-07-17 17:03:13 -0700

    less verbose setup script

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 dir=$(pwd)
-files=(.bash_profile .bash_history .bash_logout .bashrc)
+files=(.bash_profile .bash_logout .bashrc)
 cd ~/
 for f in "${files[@]}"; do 
     rm -f ${f}.OLD
-    mv $f ${f}.OLD 2>/dev/null 
+    mv $f ${f}.OLD 2>/dev/null
+	ln -s ${dir}/bash/${f:1}.sh ${f}
 done
 
-ln -s $dir/bash/bashrc.sh .bashrc
-ln -s $dir/bash/bash_profile.sh .bash_profile
-ln -s $dir/bash/bash_logout.sh .bash_logout
-ln -s $dir/bash/bash_history .bash_history
-
+mv .bash_history ${dir}/bash/bash_history 2>/dev/null
+ln -s ${dir}/bash/bash_history .bash_history
